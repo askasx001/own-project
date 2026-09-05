da12c9e2bbc243a8556a9c1aa05e9fe352719a4b
askasx001 <dasamakshaj@outlook.com>
2026-09-05 23:40:14 +0530

    fix vercel entrypoint

--- api/index.py
+++ api/index.py
@@ -1,1 +1,1 @@
-mport sys import os sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..")) from app import app # noqa: E402 
+import sys import os sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..")) from app import app as _flask_app app = _flask_app
